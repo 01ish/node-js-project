654aa11d6d31fe715aa2d5a88dd94ff19aaadd7b
Isha <124596197+01ish@users.noreply.github.com>
2024-09-05 23:50:18 +0530

    Add files via upload

---
+++ gismos/vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/gismos',
+  plugins: [react()],
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
